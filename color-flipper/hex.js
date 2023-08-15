39d107932e76f0ba03323cd11d9d6995a0cedd63
safayet <safayetdib@gmail.com>
2023-08-15 18:15:14 +0600

    added color flipper: active links

--- color-flipper/hex.js
+++ color-flipper/hex.js
@@ -1,7 +1,17 @@
+const currentPageUrl = window.location.href;
+const navLinks = document.querySelectorAll('.nav-links li a');
+
+navLinks.forEach((link) => {
+	if (link.href === currentPageUrl) {
+		link.classList.add('active');
+	}
+});
+
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+const activeLink = document.querySelector('.active');
 
 btn.addEventListener('click', () => {
 	let hexColor = '#';
@@ -13,6 +23,7 @@ btn.addEventListener('click', () => {
 
 	// color.style.color = hexColor;
 	color.textContent = hexColor;
+	activeLink.style.color = hexColor;
 });
 
 const getRandomNumber = () => {
--- color-flipper/script.js
+++ color-flipper/script.js
@@ -1,7 +1,17 @@
+const currentPageUrl = window.location.href;
+const navLinks = document.querySelectorAll('.nav-links li a');
+
+navLinks.forEach((link) => {
+	if (link.href === currentPageUrl) {
+		link.classList.add('active');
+	}
+});
+
 const colors = ['green', 'red', 'rgba(133, 122, 200)', '#f15025'];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+const activeLink = document.querySelector('.active');
 
 btn.addEventListener('click', () => {
 	// get random number between 0 and 3
@@ -13,6 +23,8 @@ btn.addEventListener('click', () => {
 
 	color.style.color = colors[randomNumber];
 	color.textContent = colors[randomNumber];
+
+	activeLink.style.color = colors[randomNumber];
 });
 
 const getRandomNumber = () => {
